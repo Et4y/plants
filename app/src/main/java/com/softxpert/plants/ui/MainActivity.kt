05dc37a439df12ac997610ea7020270c60df0cad
Hisham Ali <hi4am3ly@gmail.com>
2024-11-19 02:00:03 +0200

    add base fragment. add common toolbar

--- app/src/main/java/com/softxpert/plants/ui/MainActivity.kt
+++ app/src/main/java/com/softxpert/plants/ui/MainActivity.kt
@@ -3,16 +3,16 @@ package com.softxpert.plants.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import com.softxpert.plants.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel by viewModels<MainViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
     }
 }
 
